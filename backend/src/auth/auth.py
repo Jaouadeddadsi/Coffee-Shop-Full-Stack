6a666460cfe6d624b664aeb28216e4e1364975d6
Jaouad Eddadsi <jaouadeddadsi2016@gmail.com>
2020-05-27 21:40:46 +0000

    fix(auth): remove try execpt bock from requires_auth decorator

--- backend/src/auth/auth.py
+++ backend/src/auth/auth.py
@@ -185,13 +185,7 @@ def requires_auth(permission=''):
         @wraps(f)
         def wrapper(*args, **kwargs):
             token = get_token_auth_header()
-            try:
-                payload = verify_decode_jwt(token)
-            except:
-                raise AuthError({
-                    'code': 'token_inverified',
-                    'description': 'token can not be verified.'
-                }, 401)
+            payload = verify_decode_jwt(token)
             check_permissions(permission, payload)
             return f(payload, *args, **kwargs)
 
