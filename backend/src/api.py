7cd642659730f7a08734f0b00e968fb21eac9ce4
Jaouad Eddadsi <jaouadeddadsi2016@gmail.com>
2020-05-28 11:38:16 +0000

    build(api): implement DELETE /drinks/<id> endpoint

--- backend/src/api.py
+++ backend/src/api.py
@@ -175,6 +175,21 @@ def update_drink(payload, drink_id):
     returns status code 200 and json {"success": True, "delete": id} where id is the id of the deleted record
         or appropriate status code indicating reason for failure
 '''
+@app.route('/drinks/<int:drink_id>', methods=['DELETE'])
+@requires_auth('delete:drinks')
+def delete_drink(payload, drink_id):
+    # search for the drink in the db
+    drink = Drink.query.filter(Drink.id == drink_id).one_or_none()
+    if drink is None:
+        abort(404)
+    try:
+        drink.delete()
+        return jsonify({
+            "success": True,
+            "delete": drink_id
+        })
+    except:
+        abort(422)
 
 
 # Error Handling
