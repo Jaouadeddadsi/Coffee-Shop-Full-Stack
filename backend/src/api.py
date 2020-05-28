0a4ad20ffbd7be143bd3225a96dc44653473cfb7
Jaouad Eddadsi <jaouadeddadsi2016@gmail.com>
2020-05-28 14:12:45 +0000

    refactor(api): add endpoints comments and change the error handler for AuthError

--- backend/src/api.py
+++ backend/src/api.py
@@ -14,14 +14,6 @@ CORS(app)
 
 db_drop_and_create_all()
 
-# some test data
-drink_1 = Drink()
-drink_1.title = "drink 1"
-drink_1.recipe = json.dumps(
-    [{"color": "red", "name": "recipe 1", "parts": 1}])
-drink_1.insert()
-########
-
 # ROUTES
 '''
 GET /drinks
@@ -63,13 +55,12 @@ def drinks_detail(payload):
 
 
 '''
-@TODO implement endpoint
-    POST /drinks
-        it should create a new row in the drinks table
-        it should require the 'post:drinks' permission
-        it should contain the drink.long() data representation
+POST /drinks
+    it should create a new row in the drinks table
+    it should require the 'post:drinks' permission
+    it should contain the drink.long() data representation
     returns status code 200 and json {"success": True, "drinks": drink} where drink an array containing only the newly created drink
-        or appropriate status code indicating reason for failure
+    or appropriate status code indicating reason for failure
 '''
 @app.route('/drinks', methods=['POST'])
 @requires_auth('post:drinks')
@@ -100,15 +91,14 @@ def add_drink(payload):
 
 
 '''
-@TODO implement endpoint
-    PATCH /drinks/<id>
-        where <id> is the existing model id
-        it should respond with a 404 error if <id> is not found
-        it should update the corresponding row for <id>
-        it should require the 'patch:drinks' permission
-        it should contain the drink.long() data representation
+PATCH /drinks/<id>
+    where <id> is the existing model id
+    it should respond with a 404 error if <id> is not found
+    it should update the corresponding row for <id>
+    it should require the 'patch:drinks' permission
+    it should contain the drink.long() data representation
     returns status code 200 and json {"success": True, "drinks": drink} where drink an array containing only the updated drink
-        or appropriate status code indicating reason for failure
+    or appropriate status code indicating reason for failure
 '''
 @app.route('/drinks/<int:drink_id>', methods=['PATCH'])
 @requires_auth('patch:drinks')
@@ -141,14 +131,13 @@ def update_drink(payload, drink_id):
 
 
 '''
-@TODO implement endpoint
-    DELETE /drinks/<id>
-        where <id> is the existing model id
-        it should respond with a 404 error if <id> is not found
-        it should delete the corresponding row for <id>
-        it should require the 'delete:drinks' permission
+DELETE /drinks/<id>
+    where <id> is the existing model id
+    it should respond with a 404 error if <id> is not found
+    it should delete the corresponding row for <id>
+    it should require the 'delete:drinks' permission
     returns status code 200 and json {"success": True, "delete": id} where id is the id of the deleted record
-        or appropriate status code indicating reason for failure
+    or appropriate status code indicating reason for failure
 '''
 @app.route('/drinks/<int:drink_id>', methods=['DELETE'])
 @requires_auth('delete:drinks')
@@ -181,19 +170,7 @@ def unprocessable(error):
 
 
 '''
-@TODO implement error handlers using the @app.errorhandler(error) decorator
-    each error handler should return (with approprate messages):
-             jsonify({
-                    "success": False,
-                    "error": 404,
-                    "message": "resource not found"
-                    }), 404
-
-'''
-
-'''
-@TODO implement error handler for 404
-    error handler should conform to general task above
+error handler for 404
 '''
 @app.errorhandler(404)
 def not_found(error):
@@ -214,14 +191,13 @@ def not_found(error):
 
 
 '''
-@TODO implement error handler for AuthError
-    error handler should conform to general task above
+ error handler for AuthError
 '''
 @app.errorhandler(AuthError)
 def handle_AuthError(error):
     payload = dict(error.error)
     return jsonify({
         "success": False,
-        "error": payload["code"],
+        "error": error.status_code,
         "message": payload["description"]
     }), error.status_code
